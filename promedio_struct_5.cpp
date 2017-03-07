81b3fc025da612d77fd704b23c999e4873d998f3
kellyvasquez <kefervaza.19@hotmail.com>
2017-03-07 08:35:09 -0500

    Struct promedio mayor y menor

--- promedio_struct_5.cpp
+++ promedio_struct_5.cpp
@@ -4,18 +4,20 @@
 struct promedio {
     float nota1;
     float nota2;
-    float nota3; };
+    float nota3;
+    float prom; };
 
 struct alumno {
     char nombre[30];
     char sexo[10];
     int edad;
-    promedio prom; };
+    promedio nota[100]; };
 
 int main (){
 
     alumno alum[100];
-    int cant, prom_may, prom_men, mayor=0, menor=0;
+    int cant, mayor=0, menor=0;
+    float prom_may, prom_men;
     printf("**********REGISTRO DE ESTUDIANTES**********\n\n");
     printf("\nIngrese la cantidad de estudiantes que desea registrar: "); scanf("%d",&cant);
     system("cls");
@@ -26,45 +28,49 @@ int main (){
         printf("Ingrese el nombre %d: ", i+1); scanf("%s",alum[i].nombre);
         printf("\nIngrese el sexo (F o M): "); scanf("%s",alum[i].sexo);
         printf("\nIngrese la edad: "); scanf("%d",&alum[i].edad);
-        printf("\nIngrese nota 1: "); scanf("%f",&alum[i].prom.nota1);
-        printf("\nIngrese nota 2: "); scanf("%f",&alum[i].prom.nota2);
-        printf("\nIngrese nota 3: "); scanf("%f",&alum[i].prom.nota3);
-        printf("\nPromedio: %1.1f", (alum[i].prom.nota1+alum[i].prom.nota2+alum[i].prom.nota3)/3); }
+        printf("\nIngrese nota 1: "); scanf("%f",&alum[i].nota[i].nota1);
+        printf("\nIngrese nota 2: "); scanf("%f",&alum[i].nota[i].nota2);
+        printf("\nIngrese nota 3: "); scanf("%f",&alum[i].nota[i].nota3);
+        printf("\n-------------------------------------------\n\n");
+        alum[i].nota[i].prom=(alum[i].nota[i].nota1+alum[i].nota[i].nota2+alum[i].nota[i].nota3)/3; }
     system("cls");
 
-    prom_may=alum[0].prom;
-    prom_men=alum[0].prom;
+    prom_may=alum[0].nota[0].prom;
+    prom_men=alum[0].nota[0].prom;
 
     for(int i=0;i<cant;i++)
-    { if(prom_may<=alum[i].prom);
-        { prom_may=alum[i].prom;
+    { if(prom_may<=alum[i].nota[i].prom);
+        { prom_may=alum[i].nota[i].prom;
             mayor=i; }
     }
     for(int i=0;i<cant;i++)
-    { if(prom_men>=alum[i].prom)
-        { prom_men=alum[i].prom;
+    { if(prom_men>=alum[i].nota[i].prom)
+        { prom_men=alum[i].nota[i].prom;
             menor=i; }
     }
 
 
 
     printf("**********ESTUDIANTE CON MEJOR PROMEDIO**********\n\n");
-    printf("Nombre: %s",alum.nombre);
-    printf("\nSexo: %s",alum.sexo);
-    printf("\nEdad: %d",alum.edad);
-    printf("\nNota 1: %1.1f",alum.prom.nota1);
-    printf("\nNota 2: %1.1f",alum.prom.nota2);
-    printf("\nNota 3: %1.1f",alum.prom.nota3);
+    printf("Nombre: %s",alum[mayor].nombre);
+    printf("\nSexo: %s",alum[mayor].sexo);
+    printf("\nEdad: %d",alum[mayor].edad);
+    printf("\nNota 1: %1.1f",alum[mayor].nota[mayor].nota1);
+    printf("\nNota 2: %1.1f",alum[mayor].nota[mayor].nota2);
+    printf("\nNota 3: %1.1f",alum[mayor].nota[mayor].nota3);
+    printf("\nPromedio: %1.1f", prom_may);
     printf("\n\n");
 
     printf("**********ESTUDIANTE CON MENOR PROMEDIO**********\n\n");
-    printf("Nombre: %s",alum.nombre);
-    printf("\nSexo: %s",alum.sexo);
-    printf("\nEdad: %d",alum.edad);
-    printf("\nNota 1: %1.1f",alum.prom.nota1);
-    printf("\nNota 2: %1.1f",alum.prom.nota2);
-    printf("\nNota 3: %1.1f",alum.prom.nota3);
+    printf("Nombre: %s",alum[menor].nombre);
+    printf("\nSexo: %s",alum[menor].sexo);
+    printf("\nEdad: %d",alum[menor].edad);
+    printf("\nNota 1: %1.1f",alum[menor].nota[menor].nota1);
+    printf("\nNota 2: %1.1f",alum[menor].nota[menor].nota2);
+    printf("\nNota 3: %1.1f",alum[menor].nota[menor].nota3);
+    printf("\nPromedio: %1.1f", prom_men);
     printf("\n\n");
+
 
     return 0;
 }
