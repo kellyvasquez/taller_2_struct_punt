9cf67dfc9e9a6492182ff180fcf3586e39755474
kellyvasquez <kefervaza.19@hotmail.com>
2017-03-07 01:12:05 -0500

    Struct promedio anidado en alumno actualizado

--- promedio_anidado_struct_4.cpp
+++ promedio_anidado_struct_4.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct promedio {
     float nota1;
@@ -15,22 +16,24 @@ int main (){
 
     alumno alum;
 
-    printf("Ingrese nombre: "); scanf("%s",alum.nombre);
-    printf("Ingrese el sexo (F o M): "); scanf("%s",alum.sexo);
-    printf("Ingrese edad: "); scanf("%d",&alum.edad);
-    printf("Ingrese nota 1: "); scanf("%f",&alum.prom.nota1);
-    printf("Ingrese nota 2: "); scanf("%f",&alum.prom.nota2);
-    printf("Ingrese nota 3: "); scanf("%f",&alum.prom.nota3);
+    printf("**********DATOS DEL ESTUDIANTE**********\n\n");
+    printf("Ingrese el nombre: "); scanf("%s",alum.nombre);
+    printf("\nIngrese el sexo (F o M): "); scanf("%s",alum.sexo);
+    printf("\nIngrese la edad: "); scanf("%d",&alum.edad);
+    printf("\nIngrese nota 1: "); scanf("%f",&alum.prom.nota1);
+    printf("\nIngrese nota 2: "); scanf("%f",&alum.prom.nota2);
+    printf("\nIngrese nota 3: "); scanf("%f",&alum.prom.nota3);
+    system("cls");
 
-
-
+    printf("**********INFORMACION COMPLETA DEL ESTUDIANTE**********\n\n");
     printf("Nombre: %s",alum.nombre);
-    printf("Sexo: %s",alum.sexo);
-    printf("Edad: %d",alum.edad);
-    printf("Nota 1: %1.1f",alum.prom.nota1);
-    printf("Nota 2: %1.1f",alum.prom.nota2);
-    printf("Nota 3: %1.1f",alum.prom.nota3);
-    printf("Promedio: %1.1f", (alum.prom.nota1+alum.prom.nota2+alum.prom.nota3)/3);
+    printf("\nSexo: %s",alum.sexo);
+    printf("\nEdad: %d",alum.edad);
+    printf("\nNota 1: %1.1f",alum.prom.nota1);
+    printf("\nNota 2: %1.1f",alum.prom.nota2);
+    printf("\nNota 3: %1.1f",alum.prom.nota3);
+    printf("\nPromedio: %1.1f", (alum.prom.nota1+alum.prom.nota2+alum.prom.nota3)/3);
+    printf("\n\n");
 
     return 0;
 }
