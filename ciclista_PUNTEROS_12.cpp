1e710808a01ffc2ce292d970ce5eb4b12679581c
kellyvasquez <kefervaza.19@hotmail.com>
2017-03-14 00:55:01 -0500

    Etapas de un ciclista #12 modificado

--- ciclista_PUNTEROS_12.cpp
+++ ciclista_PUNTEROS_12.cpp
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void datos();
+
 struct etapa{
     int hora;
     int minuto;
     int seg;
-};
-
-etapa et[3];
+}et[100];
 
 int main (){
+    datos();
+}
 
+void datos(){
     int h=0, m=0, s=0;
 
     for (int i=0; i<3; i++){
-        printf("Horas: "); scanf("%d",et[i].hora);
-        printf("Minutos: "); scanf("%d",et[i].minuto);
-        printf("Segundos: "); scanf("%d",et[i].seg);
+        printf("Horas: "); scanf("%d", et[i].hora);
+        printf("Minutos: "); scanf("%d", et[i].minuto);
+        printf("Segundos: "); scanf("%d", et[i].seg);
 
-        h=h+et[i].hora;
+        /**h=h+et[i].hora;
         m=m+et[i].minuto;
         if(m>=60){
             m=m-60;
@@ -29,10 +32,9 @@ int main (){
             m++;
         }
         printf("\n");
-    }
-
+    }**/
     printf("El tiempo total en todas las etapas fue: \n\n");
     printf("Horas: %d", h);
     printf("\nMinutos: %d", m);
     printf("\nSegundos: %d", s);
-}
+}}
